48fd448b843a5d6bd04437934a38bef73b4d4143
Shaelie Spallin <shaejsp@Shaelies-MacBook-Pro.local>
2020-01-13 13:41:26 -0500

    accidentally set UI to SwiftUI instead of storyboard - fixed

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Shaelie Spallin on 2020-01-13.
+//  Copyright © 2020 Shaelie Spallin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
